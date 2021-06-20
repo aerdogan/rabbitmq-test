47d68ea0c49e80f411c50763170633d3d3904c38
aerdogan <tuxposta@gmail.com>
2021-06-21 01:54:05 +0300

    Add project files.

---
+++ Receive/Program.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Receive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connFactory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                Port = 8081
+            };
+
+            using (var connection = connFactory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (sender, eventArgs) =>
+                    {
+                        var body = eventArgs.Body.ToArray();
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine($" [X] Received {message}");
+                    };
+
+                    channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
+                    Console.WriteLine("Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
